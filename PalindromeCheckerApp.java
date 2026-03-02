7191bb0f0bf5fbba4096272118518d66451c4f13
Vaibhava392 <72888969+Vaibhava392@users.noreply.github.com>
2026-03-02 09:07:15 +0530

    Create PalindromeCheckerApp.java

---
+++ PalindromeCheckerApp.java
@@ -0,0 +1,31 @@
+class PalindromeService {
+    public boolean checkPalindrome(String input) {
+        if (input == null) return false;
+
+        // Clean the input: remove non-alphanumeric and lowercase
+        String cleanInput = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        
+        int left = 0;
+        int right = cleanInput.length() - 1;
+
+        while (left < right) {
+            if (cleanInput.charAt(left) != cleanInput.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        PalindromeService service = new PalindromeService();
+
+        String testWord = "Racecar";
+        boolean result = service.checkPalindrome(testWord);
+
+        System.out.println("Is '" + testWord + "' a palindrome? " + result);
+    }
+}
